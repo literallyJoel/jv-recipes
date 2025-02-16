e8917568d74cf3b3acd3b59e556aa009340cd3fc
Joel Vivian <35875268+literallyJoel@users.noreply.github.com>
2025-02-16 22:56:58 +0000

    Next setup (#2)
    * basic next setup

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,12 +1,5 @@
-import { Suspense } from "react";
-
 import { api, HydrateClient } from "~/trpc/server";
-import { AuthShowcase } from "./_components/auth-showcase";
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "./_components/posts";
+import { AuthButton } from "./_components/AuthButton";
 
 export default function HomePage() {
   // You can await this here if you don't want to show Suspense fallback below
@@ -17,24 +10,9 @@ export default function HomePage() {
       <main className="container h-screen py-16">
         <div className="flex flex-col items-center justify-center gap-4">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-primary">T3</span> Turbo
+            JV Recipes
           </h1>
-          <AuthShowcase />
-
-          <CreatePostForm />
-          <div className="w-full max-w-2xl overflow-y-scroll">
-            <Suspense
-              fallback={
-                <div className="flex w-full flex-col gap-4">
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                </div>
-              }
-            >
-              <PostList />
-            </Suspense>
-          </div>
+          <AuthButton />
         </div>
       </main>
     </HydrateClient>
