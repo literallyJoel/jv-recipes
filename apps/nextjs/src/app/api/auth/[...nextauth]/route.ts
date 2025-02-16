178c9db39a224281ac18f96f6260f103a08fa0a5
Joel Vivian <joel@jdvivian.co.uk>
2025-02-16 20:20:27 +0000

    final name change

--- apps/nextjs/src/app/api/auth/[...nextauth]/route.ts
+++ apps/nextjs/src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { handlers, isSecureContext } from "@jv-recipes/auth";
 
-const EXPO_COOKIE_NAME = "__acme-expo-redirect-state";
+const EXPO_COOKIE_NAME = "__jv-recipes-expo-redirect-state";
 const AUTH_COOKIE_PATTERN = /authjs\.session-token=([^;]+)/;
 
 /**
